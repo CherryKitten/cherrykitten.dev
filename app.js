60586974a08b8308783caa74bd663a7f1946afa4
CherryKitten <git@cherrykitten.dev>
2022-11-14 22:03:50 +0100

    Update basically everything

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-const mainRoutes = require("./routes/main.js");
+const bodyParser = require("body-parser");
 require("dotenv").config();
+
+const mainRoutes = require("./routes/main.js");
 
 const hostname = process.env.HOSTNAME;
 const port = process.env.PORT;
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/css", express.static(path.join(__dirname, "public/css")));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "pug");
 
 mainRoutes(app);
 
 app.listen(port, hostname, (err) => {
   if (err) console.log(err);
-  console.log(`App listening on http://${hostname}/${port}`);
+  console.log(`App listening on http://${hostname}:${port}`);
 });
--- routes/main.js
+++ routes/main.js
@@ -1,25 +1,45 @@
+const nodemailer = require("nodemailer");
 module.exports = (app) => {
-  app.get("/", (req, res) => {
-    res.render("home", {
-      title: "Home",
+  app.route("/").get((req, res) => {
+    res.render("home", { title: "Home" });
+  });
+
+  app.route("/social").get((req, res) => {
+    res.render("social", { title: "Social" });
+  });
+
+  app.route("/contact").post((req, res) => {
+    const smtpTrans = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT || 465,
+      secure: process.env.SMTP_SECURE || true,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+      },
+    });
+
+    const mailOpts = {
+      from: "CherryKitten contact form", // This is ignored by Gmail
+      to: process.env.CONTACT_MAIL || process.env.SMTP_USER,
+      subject: "New message from contact form at Cherrykitten.dev",
+      text: `${req.body.name} (${req.body.email}) :\n ${req.body.message}`,
+    };
+
+    smtpTrans.sendMail(mailOpts, (err) => {
+      if (err) {
+        res.render("social", { title: "Social", formFailed: true }); // Show a page indicating failure
+      } else {
+        res.render("social", { title: "Contact", formSent: true }); // Show a page indicating success
+      }
     });
   });
 
-  app.get("/social", (req, res) => {
-    res.render("social", {
-      title: "Social",
-    });
+  app.route("/projects").get((req, res) => {
+    res.render("projects", { title: "Projects" });
   });
 
-  app.get("/projects", (req, res) => {
-    res.render("projects", {
-      title: "Projects",
-    });
-  });
-
-  app.get("/misc", (req, res) => {
-    res.render("misc", {
-      title: "Misc",
-    });
+  app.route("/misc").get((req, res) => {
+    res.render("misc", { title: "Misc" });
   });
 };
