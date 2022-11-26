acf724eb75445e5584a9bf71fa0f775301a7e8a7
CherryKitten <git@cherrykitten.dev>
2022-11-26 11:12:42 +0100

    Final polishes

--- app.js
+++ app.js
@@ -4,18 +4,20 @@ const app = express();
 const bodyParser = require("body-parser");
 require("dotenv").config();
 
-const mainRoutes = require("./routes/main.js");
+const mainRoutes = require("./routes/main");
 
 const hostname = process.env.HOSTNAME;
 const port = process.env.PORT;
+const hcaptchasecretkey = process.env.HCAPTCHA_SECRET_KEY;
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/css", express.static(path.join(__dirname, "public/css")));
 app.use("/js", express.static(path.join(__dirname, "public/js")));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.set("view engine", "pug");
 
-mainRoutes(app);
+mainRoutes(app, hcaptchasecretkey);
 
 app.all("*", (req, res) => {
   res.render("404", { url: req.hostname + req.originalUrl });
--- routes/main.js
+++ routes/main.js
@@ -1,5 +1,7 @@
 const nodemailer = require("nodemailer");
-module.exports = (app) => {
+const {verify} = require('hcaptcha');
+
+module.exports = (app, hcaptchasecretkey) => {
   app.route("/").get((req, res) => {
     res.render("home", { title: "Home" });
   });
@@ -9,7 +11,26 @@ module.exports = (app) => {
     .get((req, res) => {
       res.render("contact", { title: "Contact" });
     })
-    .post((req, res) => {
+    .post((req, res, next) => {
+      let token = req.body['h-captcha-response'] //get hCaptcha token from form
+      if (!token) {
+        console.log('no hCaptcha token');
+        return res.render("contact", { title: "Contact", formFailed: true, message: "Captcha required."}); // Show error if no hCaptcha token provided
+      }
+          verify(hcaptchasecretkey, token) //Verify the token
+              .then((data) => {
+                if (data.success === true) {
+                  console.log('success!', data);
+                } else {
+                  console.log('verification failed');
+                }
+              })
+              .catch(console.error);
+      next()
+        },
+        (req, res) =>
+    {
+      //All the data for sending a mail
       const smtpTrans = nodemailer.createTransport({
         host: process.env.SMTP_HOST,
         port: process.env.SMTP_PORT || 465,
@@ -19,21 +40,21 @@ module.exports = (app) => {
           pass: process.env.SMTP_PASS,
         },
       });
-
+      //Build email
       const mailOpts = {
-        from: "CherryKitten contact form <admin@cherrykitten.dev>", // This is ignored by Gmail
+        from: "CherryKitten contact form <" + process.env.SMTP_USER + ">",
         to: process.env.CONTACT_MAIL || process.env.SMTP_USER,
         subject: "New message from contact form at Cherrykitten.dev",
         text: `${req.body.name} (${req.body.email}) :\n ${req.body.message}`,
       };
-
+      //Send mail
       smtpTrans.sendMail(mailOpts, (err) => {
         if (err) {
           console.log(err);
-          res.render("contact", { title: "Social", formFailed: true }); // Show a page indicating failure
+          res.render("contact", { title: "Contact", formFailed: true, message: "Could not Submit. Please try again later. The error has been logged and will be investigated." }); // Show error
         } else {
           console.log("Email sent!");
-          res.render("contact", { title: "Contact", formSent: true }); // Show a page indicating success
+          res.render("contact", { title: "Contact", formSent: true }); // Show success page
         }
       });
     });
@@ -50,9 +71,6 @@ module.exports = (app) => {
     res.render("about_certifications", { title: "About me" });
   });
 
-  app.route("/blog").get((req, res) => {
-    res.render("blog", { title: "Blog" });
-  });
   app.route("/impressum").get((req, res) => {
     res.render("impressum", { title: "Impressum" });
   });
